db5b60126ea493263601a5681bbcc6b297d97d62
Shubham Chitrakar <chitrakar05@gmail.com>
2026-02-04 22:30:31 +0530

    Add files via upload

---
+++ Final_variable.java
@@ -0,0 +1,11 @@
+public class Final_variable {
+    public static void main(String[]args){
+        
+        
+        
+        final float pi=3.14f;
+        int r = 3;
+        float area = pi*r*r;
+        System.out.println("area of circle: "+area);
+    }
+}
